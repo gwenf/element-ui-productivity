390124e22105ab12748c7f24f32690d3e1e24a52
gwenf <gwenfaraday@pm.me>
2020-04-16 08:50:27 -0400

    first commit; added element ui cli plugin; initial setup

---
+++ src/components/TopNav.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Nav</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-page">
+    Home Page
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
