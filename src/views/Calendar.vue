9069cc9715660b70ac92a0e2fab61fb0a890a7e1
gwenf <gwenfaraday@pm.me>
2020-04-29 20:05:34 -0400

    final updates

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,4 +1,7 @@
 <template>
-  <el-calendar :range="['2019-03-04', '2019-03-24']">
-  </el-calendar>
+  <el-row>
+    <el-col :span="18" :offset="3">
+      <el-calendar :range="['2019-03-04', '2019-03-24']" />
+    </el-col>
+  </el-row>
 </template>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,12 +1,66 @@
 <template>
-  <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>Card name</span>
-      <el-button style="float: right; padding: 3px 0" type="text">Submit</el-button>
-    </div>
+  <el-row>
+    <el-col :span="18" :offset="3">
+      <div class="card-wrapper">
+        <el-card class="box-card" v-for="(card, i) in cards" :key="i">
+          <div slot="header" class="card-header">
+            <span>Add Task</span>
+            <el-button type="text">Submit</el-button>
+          </div>
 
-    <div class="text item">
-      Form Section
-    </div>
-  </el-card>
+          <div class="text item">
+            <el-input placeholder="Name" v-model="inputs[i]" />
+            <el-input
+              type="textarea"
+              placeholder="Description"
+              v-model="textareas[i]"
+              maxlength="30"
+              show-word-limit
+            />
+          </div>
+          <!-- <el-button type="primary" @click="onSubmit">Create</el-button>
+          <el-button>Cancel</el-button> -->
+        </el-card>
+      </div>
+    </el-col>
+  </el-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [...Array(1).keys()],
+      inputs: [],
+      textareas: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.card-header button {
+  padding: 2px 5px;
+  margin: 0;
+}
+.card-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px;
+}
+.el-card {
+  width: 300px;
+  margin: 5px;
+}
+.el-textarea {
+  margin-top: 10px;
+}
+.el-button {
+  margin-top: 10px;
+}
+</style>
